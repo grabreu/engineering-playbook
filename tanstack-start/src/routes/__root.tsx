77ca9381a9ae45ccf850bcea9b03d61aa22376c7
Gabriel Abreu <gabriel.roberto.abreu@outlook.com.br>
2026-09-01 10:28:25 -0300

    chore: scaffold tanstack start

---
+++ tanstack-start/src/routes/__root.tsx
@@ -0,0 +1,91 @@
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import type { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  createRootRouteWithContext,
+  HeadContent,
+  Scripts,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+import type { ReactNode } from "react";
+import appCss from "~/styles/app.css?url";
+import { seo } from "~/utils/seo";
+
+type RootDocumentProps = {
+  children: ReactNode;
+};
+
+const RootDocument = ({ children }: RootDocumentProps) => {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <TanStackDevtools
+          plugins={[
+            {
+              name: "Tanstack Router",
+              render: <TanStackRouterDevtoolsPanel />,
+            },
+            {
+              name: "Tanstack Query",
+              render: <ReactQueryDevtools />,
+            },
+          ]}
+        />
+        <Scripts />
+      </body>
+    </html>
+  );
+};
+
+type RootRouteContext = {
+  queryClient: QueryClient;
+};
+
+export const Route = createRootRouteWithContext<RootRouteContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        name: "theme-color",
+        content: "#ffffff",
+      },
+      ...seo({
+        title: "Todo App | TanStack Start",
+        description: "A sample todo application built with TanStack Start.",
+      }),
+    ],
+    links: [
+      { rel: "stylesheet", href: appCss },
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/apple-touch-icon.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      { rel: "manifest", href: "/site.webmanifest" },
+      { rel: "icon", href: "/favicon.ico" },
+    ],
+  }),
+  shellComponent: RootDocument,
+});
